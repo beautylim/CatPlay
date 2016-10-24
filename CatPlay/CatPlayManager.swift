d5166e4840cc538af93cccc6a5d76e0bd187cfc3
li.min <li.min@adways.cn>
2016-10-24 18:14:56 +0800

    添加上传文件和图片

--- CatPlay/CatPlayManager.swift
+++ CatPlay/CatPlayManager.swift
@@ -7,31 +7,30 @@
 //
 
 import Foundation
+let uploadImageHostURL:String = "http://www.freeimagehosting.net/upload.php"//上传图片
+let uploadDataHostURL:String = "http://jsonplaceholder.typicode.com/posts"//上传文件
 public enum HTTPMethod:String{
     case GET  = "GET"
     case POST = "POST"
+    case PUT  = "PUT"
 }
 
 enum HTTPErrorCode:Int{
     case unknowReason = 0000
     case nilResponseData = 0001
+    case failureSerializer = 0002
 }
 
-class CatPlayManager{
-    var hostURL:String {
-        get {
-            return "http://Redmine.adways.cn/redmine"//需要自己改
-        }
-        set{
-            self.hostURL = newValue
-        }
-    }
+class CatPlayManager:NSObject, URLSessionTaskDelegate{
+    let hostURL:String = "http://Redmine.adways.cn/redmine"
     static let shareInstance = CatPlayManager()
-    fileprivate init(){
+    var uploadDataProgress:((Double)->())?
+    fileprivate override init(){
         
     }
     
-    func request(url urlString:String,method:HTTPMethod,parameter:[String:Any]?=nil,header:[String:String]?=nil,Success:@escaping (Data,URLResponse)->(),failure:@escaping (HTTPError)->()){
+    
+    func request(url urlString:String,method:HTTPMethod,parameter:[String:Any]?=nil,header:[String:String]?=nil,success:((Any)->())?=nil,failure:((HTTPError)->())?=nil){
         //1.生成configuration
         let defaultConfiguration = URLSessionConfiguration.default
         
@@ -57,8 +56,6 @@ class CatPlayManager{
             httpRequest.allHTTPHeaderFields = httpHeader
         }
         httpRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-        httpRequest.setValue("CatPlay" + version, forHTTPHeaderField: "User-Agent")
         httpRequest.httpMethod = method.rawValue
         if method == .POST,let parameterDic = parameter {
             var httpBodyString = ""
@@ -70,18 +67,81 @@ class CatPlayManager{
         }
         
         //5.生成URLSessiontask
-        let sessionTask = session.dataTask(with: httpRequest) { (responseData, urlResponse, error) in
-            guard error == nil else { return failure(HTTPError.init(reason: "有不明错误", statusCode:HTTPErrorCode.unknowReason))}
-            guard let data = responseData , data.count > 0 else {
-               return failure(HTTPError.init(reason: "什么都没有返回", statusCode: HTTPErrorCode.nilResponseData))
+        let sessionTask = session.dataTask(with: httpRequest) {(responseData, urlResponse, error) in
+            let result:CatPlayResponseResult = self.responseJSONSerializer(responseData: responseData, response: urlResponse, error: error)
+            switch result{
+            case .success(let response):
+                success!(response)
+            case .failure(let catPlayError):
+                failure!(catPlayError)
             }
-            Success(responseData!,urlResponse!)
-            
-            
         }
         
         sessionTask.resume()
     }
+    
+    func upload(method:HTTPMethod,url:String,contentType:String,acceptType:String,fromData:()->(Data),progress:((Double)->())?=nil,success:((Any)->())?=nil,failure:((HTTPError)->())?=nil){
+        
+        self.uploadDataProgress = progress
+        //1.生成configuration
+        let defaultConfiguration = URLSessionConfiguration.default
+        
+        //2.生成session
+        let session = URLSession.init(configuration: defaultConfiguration, delegate: self, delegateQueue: nil)
+        
+        //3.生成url
+        let encodingURL = url.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+        let httpURL = URL.init(string: encodingURL!)
+        
+        //4.生成URLRequest
+        var httpRequest = URLRequest.init(url: httpURL!)
+        httpRequest.httpMethod = method.rawValue
+        
+        //这些两个一定要设置
+        httpRequest.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        httpRequest.setValue(acceptType, forHTTPHeaderField: "Accept")
+        httpRequest.cachePolicy = .reloadIgnoringCacheData
+        httpRequest.timeoutInterval = 20
+        
+        let uploadData = fromData()
+        let uploadTask = session.uploadTask(with: httpRequest, from: uploadData) { (responseData, urlResponse, error) in
+            let result:CatPlayResponseResult = self.responseJSONSerializer(responseData: responseData, response: urlResponse, error: error)
+            switch result{
+            case .success(let response):
+                success!(response)
+            case .failure(let catPlayError):
+                failure!(catPlayError)
+            }
+            
+        }
+        uploadTask.resume()
+    }
+    
+    func responseJSONSerializer(responseData:Data?,response:URLResponse?,error:Error?)->CatPlayResponseResult{
+        print(response!)
+        guard error == nil else { return CatPlayResponseResult.failure(HTTPError.init(reason: "有不明错误", statusCode:HTTPErrorCode.unknowReason))}
+        guard let data = responseData , data.count > 0 else {
+            return CatPlayResponseResult.failure(HTTPError.init(reason: "什么都没有返回", statusCode: HTTPErrorCode.nilResponseData))
+        }
+        do{
+            let json = try JSONSerialization.jsonObject(with: data, options:.allowFragments)
+            return CatPlayResponseResult.success(json)
+        }catch{
+            return CatPlayResponseResult.failure(HTTPError.init(reason: "解析失败", statusCode:HTTPErrorCode.failureSerializer))
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
+        if let progress = self.uploadDataProgress{
+            let nowPercent = Double(totalBytesSent) / Double(totalBytesExpectedToSend)
+            progress(nowPercent)
+        }
+    }
+}
+
+enum CatPlayResponseResult{
+    case success(Any)
+    case failure(HTTPError)
 }
 
 class HTTPError {
--- CatPlay/ViewController.swift
+++ CatPlay/ViewController.swift
@@ -7,26 +7,33 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
-        CatPlayManager.shareInstance.request(url: "issues.json", method: .GET, parameter: ["assign_to":"me"], header: getAuthDic(), Success: { (data, response) in
-            
-            do{
-                let json = try JSONSerialization.jsonObject(with: data, options:.allowFragments)
-                print(json)
-            }catch let error as NSError{
-                print(error)
-            }
+        CatPlayManager.shareInstance.request(url: "issues.json", method: .GET, parameter: ["assign_to":"me"], header: getAuthDic(), success: { (response) in
             
                 print("success connect!")
             }) { (error) in
                 print("\(error.reason)  and  \(error.statusCode)")
         }
+        
+        //上传图片
+        
+        CatPlayManager.shareInstance.upload(method: .POST, url:uploadImageHostURL, contentType: "image/jpeg", acceptType: "application/json", fromData: { () -> (Data) in
+            let uploadImage = UIImage.init(named: "image_01")
+            return UIImageJPEGRepresentation(uploadImage!, 1.0)!
+            },progress: {percent in
+                print("已经上传了\(percent)")
+            }, success: { (response) in
+                print("success upload image")
+            }) { (error) in
+                print("\(error.reason)  and  \(error.statusCode)")
+        }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,8 +41,8 @@ class ViewController: UIViewController {
     }
 
     fileprivate func setHttpAuth()->String{
-        let userAccount = "lalala"//需要自己改
-        let password = "lalaala"//需要自己改
+        let userAccount = "yang.hao"//需要自己改
+        let password = "11111111"//需要自己改
         let str = NSString.init(string: "\(userAccount):\(password)")
         let data = str.data(using: String.Encoding.utf8.rawValue)
         let base64 = data?.base64EncodedString(options: .lineLength64Characters)
