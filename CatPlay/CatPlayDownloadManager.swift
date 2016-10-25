b14e047ea3a8c02ed060bda9e65d2e56876539d1
li.min <li.min@adways.cn>
2016-10-25 14:56:26 +0800

    断点下载

---
+++ CatPlay/CatPlayDownloadManager.swift
@@ -0,0 +1,99 @@
+//
+//  CatPlayDownloadManager.swift
+//  CatPlay
+//
+//  Created by li.min on 2016/10/25.
+//  Copyright © 2016年 li.min. All rights reserved.
+//
+
+import Foundation
+
+class CatPlayDownloadManager:NSObject,URLSessionDownloadDelegate{
+    var resumeData:Data?
+    var task:URLSessionDownloadTask!
+    var session:URLSession!
+    var urlRequest:URLRequest!
+    var progress:((Float)->())?
+    override init(){
+        super.init()
+    }
+    
+    func start(){
+        print("开始下载")
+        initTask()
+        task = session.downloadTask(with: urlRequest)
+        task.resume()
+    }
+    
+    func suspend(){
+        print("暂停下载")
+
+        guard task == nil else{
+            return task.cancel { (data) in
+                self.resumeData = data
+            }
+        }
+    }
+    
+    func resume(){
+         print("继续下载")
+        if let data = resumeData {
+            task = session.downloadTask(withResumeData: data)
+            task.resume()
+        }
+    }
+    
+    func cancle(){
+        print("取消下载")
+        guard task == nil else {
+            return task.cancel()
+        }
+    }
+    
+    func initTask(){
+        let configuration = URLSessionConfiguration.default
+        session = URLSession.init(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+        let urlString = "http://www.1ting.com/api/audio?/i/1031/7/14.mp3"
+        let encodingString = urlString.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+        let url = URL.init(string: encodingString!)
+        urlRequest = URLRequest.init(url: url!, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 20)
+        
+    }
+    
+    //暂停，取消都会调用的方法，获取resumeData
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let myerror = error {
+            print("didcomplete"+myerror.localizedDescription)
+        }
+    }
+    
+    //完成下载，移动下载文件
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("下载结束")
+        let locationPath = location.path
+        print(locationPath)
+        let documents = NSHomeDirectory() + "/Documents/14.mp3"
+        print(documents)
+        let defaultFile = FileManager.default
+        do {
+            try defaultFile.moveItem(atPath: locationPath, toPath: documents)
+            try defaultFile.removeItem(atPath: documents)
+        }catch let error {
+            print("didFinish"+error.localizedDescription)
+        }
+        
+        }
+    
+    //开始断点下载调用
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
+        
+    }
+    
+    //正在下载
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let finishedPRogress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        progress?(finishedPRogress)
+    }
+    
+    
+}
--- CatPlay/ViewController.swift
+++ CatPlay/ViewController.swift
@@ -9,32 +9,60 @@
 import UIKit
 class ViewController: UIViewController {
 
+    var downLoadManager:CatPlayDownloadManager = CatPlayDownloadManager()
+    
+    
+    @IBOutlet weak var progressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
-        CatPlayManager.shareInstance.request(url: "issues.json", method: .GET, parameter: ["assign_to":"me"], header: getAuthDic(), success: { (response) in
-            
-                print("success connect!")
-            }) { (error) in
-                print("\(error.reason)  and  \(error.statusCode)")
+        downLoadManager.progress = {progress in
+            self.progressLabel.text = "已经下载了 \(progress)"
         }
+        // Do any additional setup after loading the view, typically from a nib.
+//        CatPlayManager.shareInstance.request(url: "issues.json", method: .GET, parameter: ["assign_to":"me"], header: getAuthDic(), success: { (response) in
+//            
+//                print("success connect!")
+//            }) { (error) in
+//                print("\(error.reason)  and  \(error.statusCode)")
+//        }
         
         //上传图片
         
-        CatPlayManager.shareInstance.upload(method: .POST, url:uploadImageHostURL, contentType: "image/jpeg", acceptType: "application/json", fromData: { () -> (Data) in
-            let uploadImage = UIImage.init(named: "image_01")
-            return UIImageJPEGRepresentation(uploadImage!, 1.0)!
-            },progress: {percent in
-                print("已经上传了\(percent)")
-            }, success: { (response) in
-                print("success upload image")
-            }) { (error) in
-                print("\(error.reason)  and  \(error.statusCode)")
-        }
+//        CatPlayManager.shareInstance.upload(method: .POST, url:uploadImageHostURL, contentType: "image/jpeg", acceptType: "text/html", fromData: { () -> (Data) in
+//            let uploadImage = UIImage.init(named: "image_01")
+//            return UIImageJPEGRepresentation(uploadImage!, 1.0)!
+//            },progress: {percent in
+//                print("已经上传了\(percent)")
+//            }, success: { (response) in
+//                print("success upload image")
+//            }) { (error) in
+//                print("\(error.reason)  and  \(error.statusCode)")
+//        }
+        
+        //断点下载
+        
+    
     }
     
+    @IBAction func start(_ sender: AnyObject) {
+        downLoadManager.start()
+    }
 
+    
+    @IBAction func suspend(_ sender: AnyObject) {
+        downLoadManager.suspend()
+    }
+    
+    
+    @IBAction func resume(_ sender: AnyObject) {
+        downLoadManager.resume()
+    }
+    
+    @IBAction func cancle(_ sender: AnyObject) {
+        downLoadManager.cancle()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
